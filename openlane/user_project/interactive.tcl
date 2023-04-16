531daedab377c1ef33e2452a73d1c41e420c6734
mo-hosni <m.hosni.ibra@gmail.com>
2023-04-16 05:42:37 -0700

    reharden `user_project`

--- openlane/user_project/interactive.tcl
+++ openlane/user_project/interactive.tcl
@@ -16,7 +16,7 @@
 package require openlane
 set script_dir [file dirname [file normalize [info script]]]
 
-prep -design $script_dir -tag user_project -overwrite
+prep -design $script_dir -tag user_project -overwrite -ignore_mismatches
 set save_path $script_dir/../..
 
 verilog_elaborate
